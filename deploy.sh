34fac3a9f705b23f0a155180c41a8ea6ecd0f6f3
fBrx <admin@florian-m.net>
2015-01-03 14:39:28 +0100

    purge target repo before deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,19 @@
+# purge and rebuild site
 rm -rf public && hugo
+
+# copy to correct repo for publishing
+rm -rf ../fBrx.github.io/*
 cp -rf public/* ../fBrx.github.io/
+
+## switch to publishing repo
 cd ../fBrx.github.io
-git add -A
-git status
-git commit -m "updated site content"
-git push
+
+# add all changes
+git add -A && git status
+
+# commit and push updated content
+message="updated site content"
+if [ $# -eq 1 ]
+    then message="$1"
+fi
+git commit -m "$message" && git push
